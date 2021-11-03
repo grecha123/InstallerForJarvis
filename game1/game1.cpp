23fb27fc4cef928b1140e15a05c0476bb5593552
grecha123 <93035704+grecha123@users.noreply.github.com>
2021-11-03 15:16:24 +0300

    first commit

---
+++ game1/game1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+void  player1();
+int a;
+int b;
+ 
+int main()
+{
+    setlocale(LC_ALL, "RUSSIAN");
+    player1();
+}
+void player1()
+{
+    cout << "fasfasfsafsafasfsaf";
+    cin >> a;
+    cout << "игрок 2 ввел число " <<a <<endl;
+    system("pause");
+}
